cc27a2b8fccafd8415e68c1f791045952d54425a
Zach B <56662213+zachb1807@users.noreply.github.com>
2025-01-28 16:22:51 -0600

    remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-[url(/identity-beams.png)] bg-cover place-content-center w-full h-full flex flex-col justify-center align-middle items-center content-center font-[family-name:var(--font-geist-sans)]">
       <h1 className={'text-4xl align-middle font-semibold tracking-tight'}>coming soon</h1>
       <div className="h-5"/>
-      <p>zach buchar's website</p>
+      <p>zach buchar&apos;s website</p>
     </div>
   );
 }
