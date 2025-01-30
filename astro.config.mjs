23d5154e42ab89d3aeb4d120362caecf1124d082
Zach B <56662213+zachb1807@users.noreply.github.com>
2025-01-30 08:34:16 -0600

    add netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-sphere-demo.vercel.app",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({
     applyBaseStyles: false
-  }), react()]
+  }), react()],
+  output: "server",
+  adapter: netlify()
 });
